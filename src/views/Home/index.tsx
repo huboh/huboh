32738e79aba7361e98b7cf28a88d593648155963
Huboh <72440222+huboh@users.noreply.github.com>
2022-06-17 16:44:33 +0100

    🙂CHORE: add boilerplate code for `Home` view

---
+++ src/views/Home/index.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+
+// import Header from "../../components/Header";
+// import Footer from "../../components/Footer";
+
+
+const Home: FC = () => (
+  <>
+    {/* <Header /> */ }
+    <main className="view home-view">hello</main>
+    {/* <Footer /> */ }
+  </>
+);
+
+
+export {
+  Home as default
+};
