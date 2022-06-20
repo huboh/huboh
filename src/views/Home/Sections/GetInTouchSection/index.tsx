a74a72aed4a616b1b8c03097bce7013a3eecb47b
Huboh <72440222+huboh@users.noreply.github.com>
2022-06-20 21:02:45 +0100

    🙄 CHORE: create boilerplate code for sub-component `GetInTouchSection` under `Home` view

---
+++ src/views/Home/Sections/GetInTouchSection/index.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+import Section from "../../../../components/Section";
+
+export interface GetInTouchSectionProps { }
+
+
+const GetInTouchSection: FC<GetInTouchSectionProps> = () => (
+  <Section id="get-in-touch">
+    get in touch section
+  </Section>
+);
+
+
+export {
+  GetInTouchSection as default
+};
