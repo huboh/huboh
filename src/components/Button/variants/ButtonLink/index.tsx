b04ec356395679750a89a424d63988a78cea71b6
Huboh <72440222+huboh@users.noreply.github.com>
2022-06-20 19:24:22 +0100

    🤘CHORE: create variant button component `ButtonLink`

---
+++ src/components/Button/variants/ButtonLink/index.tsx
@@ -0,0 +1,23 @@
+import './button-link.scss';
+import { FC, ReactNode } from 'react';
+import { Link, LinkProps } from 'react-router-dom';
+import { ButtonStyle } from '../Button';
+
+export interface ButtonLinkProps extends LinkProps {
+  text: string;
+  icon?: ReactNode;
+  linkStyle?: ButtonStyle;
+}
+
+const ButtonLink: FC<ButtonLinkProps> = ({ text, to, icon, linkStyle, children, ...rest }) => (
+  <Link { ...rest } className={ `button button-link ${linkStyle || ''}`.trim() } to={ to } >
+    { icon && <span className="icon-wrapper">{ icon }</span> }
+    <span className='text-wrapper'>
+      { text || children }
+    </span>
+  </Link>
+);
+
+export {
+  ButtonLink as default
+};
