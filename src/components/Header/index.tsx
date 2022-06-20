38704ddc03cc6b43f6c2da53fed3dd26b49b5981
Huboh <72440222+huboh@users.noreply.github.com>
2022-06-20 19:51:18 +0100

    🙊 CHORE: add components - `Logo` & `Navbar` to `Header` component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,14 @@
 import "./header.scss";
 import { FC } from "react";
 
+import Logo from "../Logo";
+import Navbar from "./Navbar";
+
 
 const Header: FC = () => (
   <header className="header">
+    <Logo />
+    <Navbar />
   </header>
 );
 
