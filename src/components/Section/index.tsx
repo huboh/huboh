93a02221de804bc144d90d0a12293f745bd9819d
Huboh <72440222+huboh@users.noreply.github.com>
2022-06-24 18:53:18 +0100

    🤙REFACTOR: add support to change text alignment & properly concatenate html class attribute values for `Section` component

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,17 +1,27 @@
+import "./section.scss";
+
 import { FC, ReactNode } from "react";
+import { joinClassStrings } from "../../utils";
+
+export type TextAlign = "left" | "center" | "right";
 
 export interface SectionProps {
+  textAlignment?: TextAlign;
   children?: ReactNode;
   className?: string;
   id?: string;
 }
 
 
-const Section: FC<SectionProps> = ({ children, className, id }) => (
-  <section className={ "section" + (className || "") } id={ id }>
-    { children }
-  </section>
-);
+const Section: FC<SectionProps> = ({ children, id, ...props }) => {
+  const className = props.className || "";
+  const textAlignment = props.textAlignment || "";
+  const classString = joinClassStrings(textAlignment, className);
+
+  return (
+    <section className={ `section ${classString}`.trim() } id={ id } children={ children } />
+  );
+};
 
 
 export {
