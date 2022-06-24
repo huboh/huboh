c79ff639bdce95d0cbfd32a9588c2dec95e4d0ca
Huboh <72440222+huboh@users.noreply.github.com>
2022-06-24 18:42:24 +0100

    🔨FIX: add support for `Link` component to accept class attribute values

--- src/components/Button/variants/ButtonLink/index.tsx
+++ src/components/Button/variants/ButtonLink/index.tsx
@@ -1,7 +1,9 @@
 import './button-link.scss';
 import { FC, ReactNode } from 'react';
 import { Link, LinkProps } from 'react-router-dom';
+
 import { ButtonStyle } from '../Button';
+import { joinClassStrings } from "../../../../utils";
 
 export interface ButtonLinkProps extends LinkProps {
   text: string;
@@ -9,14 +11,20 @@ export interface ButtonLinkProps extends LinkProps {
   linkStyle?: ButtonStyle;
 }
 
-const ButtonLink: FC<ButtonLinkProps> = ({ text, to, icon, linkStyle, children, ...rest }) => (
-  <Link { ...rest } className={ `button button-link ${linkStyle || ''}`.trim() } to={ to } >
-    { icon && <span className="icon-wrapper">{ icon }</span> }
-    <span className='text-wrapper'>
-      { text || children }
-    </span>
-  </Link>
-);
+const ButtonLink: FC<ButtonLinkProps> = ({ text, to, icon, children, ...rest }) => {
+  const linkStyle = rest.linkStyle || "";
+  const className = rest.className || "";
+  const classString = joinClassStrings(linkStyle, className);
+
+  return (
+    <Link { ...rest } className={ `button button-link ${classString}`.trim() } to={ to } >
+      { icon && <span className="icon-wrapper">{ icon }</span> }
+      <span className='text-wrapper'>
+        { text || children }
+      </span>
+    </Link>
+  );
+};
 
 export {
   ButtonLink as default
