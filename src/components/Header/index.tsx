2d3aac1a4cc82f8125f7bc337fc719a4246fdce0
Huboh <72440222+huboh@users.noreply.github.com>
2022-06-26 13:22:50 +0100

    😁 FEAT: made header sticky only when scrolling up

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,43 @@
 import "./header.scss";
-import { FC } from "react";
+import { FC, useRef } from "react";
 
 import Logo from "../Logo";
 import Navbar from "./Navbar";
 
+import useEventListener from "../../hooks/useEventListener";
 
-const Header: FC = () => (
-  <header className="header">
-    <Logo />
-    <Navbar />
-  </header>
-);
+
+const Header: FC = () => {
+  const headerRef = useRef<HTMLElement>(null);
+  const lastScrollPositionRef = useRef(0);
+
+  useEventListener({
+    target: window,
+    eventType: "scroll",
+    eventHandler: async (event) => {
+      const window = event.currentTarget as Window;
+      const currentScrollPosition = window.scrollY;
+      const lastScrollPosition = lastScrollPositionRef.current;
+
+      const isScrolledUp = currentScrollPosition <= 30;
+      const isScrollingUp = currentScrollPosition < lastScrollPosition;
+      const isScrollingDown = currentScrollPosition > lastScrollPosition;
+
+      headerRef.current?.classList?.toggle?.("scrolled-up", isScrolledUp);
+      headerRef.current?.classList?.toggle?.("scrolling-up", isScrolledUp ? !isScrolledUp : isScrollingUp);
+      headerRef.current?.classList?.toggle?.("scrolling-down", isScrollingDown);
+
+      lastScrollPositionRef.current = currentScrollPosition;
+    }
+  });
+
+  return (
+    <header className="header" ref={ headerRef }>
+      <Logo />
+      <Navbar />
+    </header>
+  );
+};
 
 
 export {
