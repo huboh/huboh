ec7fa2fac75500040eb346f2927e681bc0e58266
Huboh <72440222+huboh@users.noreply.github.com>
2022-06-28 12:09:28 +0100

    👀 CHORE: add `IconLink` component as a property on `Button` component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,5 @@
 import Icon from "./variants/Icon";
+import IconLink from "./variants/IconLink";
 import ButtonLink from "./variants/ButtonLink";
 import ButtonDefault, { ButtonProps } from "./variants/Button";
 
@@ -10,3 +11,4 @@ export default function Button(props: ButtonProps) {
 
 Button.Icon = Icon;
 Button.Link = ButtonLink;
+Button.IconLink = IconLink;
