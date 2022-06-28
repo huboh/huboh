5cd56d21da58d98f0bf9b962e08398d905806d70
Huboh <72440222+huboh@users.noreply.github.com>
2022-06-28 12:50:57 +0100

    🙂 REFACTOR: add sub components as properties on `Text` component

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,4 +1,7 @@
+import Link from "./components/Link";
+import List from "./components/List";
 import Header from "./components/Header";
+import ColoredText from "./components/ColoredText";
 import Paragraph, { ParagraphProps } from "./components/Paragraph";
 
 export default function Text(props: ParagraphProps) {
@@ -7,5 +10,8 @@ export default function Text(props: ParagraphProps) {
   );
 }
 
+Text.Link = Link;
+Text.List = List;
 Text.Header = Header;
+Text.Colored = ColoredText;
 Text.Paragraph = Paragraph;
