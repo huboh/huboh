09058286c9a2078c71ca528a707d70d0e0fb5140
Huboh <72440222+huboh@users.noreply.github.com>
2022-06-28 13:08:48 +0100

    😬 REFACTOR: add text to `Footer` component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,15 @@
+import "./footer.scss";
+
 import { FC } from "react";
+import Text from "../Text";
 
 
 const Footer: FC = () => (
-  <footer>
+  <footer className="footer">
+    <Text.Paragraph size="small">
+      <span>Designed & Built with 💖 by </span>
+      <a className="link" href="https://www.twitter.com" target="_blank" rel="noreferrer" >Musa Knowledge</a>
+    </Text.Paragraph>
   </footer>
 );
 
