e7edb8b1cc31d3bac550af8c296193b92a04a40e
Huboh <72440222+huboh@users.noreply.github.com>
2022-07-03 19:18:29 +0100

    😬 CHORE: add new constants `themes` & `NavigationLinks`

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,17 @@
+import { Preferences } from "../providers/PreferencesProvider/types";
+
+
 export const ResumeFileName = "resume";
 export const ResumeDownloadLink = "/resume.pdf";
 
 export const defaultListBulletIcon = "⚡";
 
 export const preferencesKey = "preferences";
+
+export const themes: Preferences['theme'][] = ["light", "dark", "system"];
+
+export const NavigationLinks = [
+  { label: "about", link: "#about" },
+  { label: "recent projects", link: "#recent-project" },
+  { label: "contact", link: "#contact" },
+];
