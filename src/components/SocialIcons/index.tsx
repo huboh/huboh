bbb9adebdf218204f740f5666732ff49b512156b
Huboh <72440222+huboh@users.noreply.github.com>
2022-07-08 20:43:31 +0100

    CHORE: create component `SocialIcons` to wrap social icons

---
+++ src/components/SocialIcons/index.tsx
@@ -0,0 +1,25 @@
+import "./social-icons.scss";
+
+import { FC } from "react";
+import { SocialLinks } from "../../constants";
+import { TbBrandLinkedin, TbBrandTwitter, TbBrandGithub } from "react-icons/tb";
+
+import Button from "../Button";
+
+const socialIcons = [
+  { icon: <TbBrandGithub />, link: SocialLinks.github },
+  { icon: <TbBrandTwitter />, link: SocialLinks.twitter },
+  { icon: <TbBrandLinkedin />, link: SocialLinks.linkedIn },
+];
+
+const SocialIcons: FC = () => (
+  <ul className="social-icons-wrapper">
+    { socialIcons.map(socialIcon => (
+      <li className="social-icon-wrapper" children={ <Button.IconLink to={ socialIcon.link } icon={ socialIcon.icon } /> } />
+    )) }
+  </ul>
+);
+
+export {
+  SocialIcons as default
+};
