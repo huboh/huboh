9a6e81b807d03d6acc99d7c42fec31bd738cd625
Huboh <72440222+huboh@users.noreply.github.com>
2022-07-08 20:45:03 +0100

    REFACTOR: add `SocialIcons` component to `Footer` component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,20 +1,29 @@
 import "./footer.scss";
 
 import { FC } from "react";
+import { SocialLinks } from "../../constants";
 
 import Text from "../Text";
 import ScrollToTop from "../ScrollToTop";
+import SocialIcons from "../SocialIcons";
 
 
-const Footer: FC = () => (
-  <footer className="footer">
-    <Text.Paragraph size="small">
-      <span>Designed & Built with 💖 by </span>
-      <a className="link" href="https://www.twitter.com/from_Godfather" target="_blank" rel="noreferrer" >Musa Knowledge</a>
-    </Text.Paragraph>
-    <ScrollToTop />
-  </footer>
-);
+const Footer: FC = () => {
+  const paragraphText = (
+    <>
+      <span children="Designed & Built with 💖 by " />
+      <a className="link" target="_blank" rel="noreferrer" children="Musa Knowledge" href={ SocialLinks.twitter } />
+    </>
+  );
+
+  return (
+    <footer className="footer">
+      <SocialIcons />
+      <Text.Paragraph size="small" children={ paragraphText } />
+      <ScrollToTop />
+    </footer>
+  );
+};
 
 
 export {
