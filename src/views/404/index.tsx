0b3d80ece5915a9c7b7ba64b74ec059cfe54a8bb
Huboh <72440222+huboh@users.noreply.github.com>
2022-07-09 20:10:41 +0100

    CHORE: add new view -> `404`

---
+++ src/views/404/index.tsx
@@ -0,0 +1,46 @@
+import "./styles/404.scss";
+
+import { FC } from "react";
+import { TbHome } from "react-icons/tb";
+import { HOME } from "../../constants/routes";
+
+// components
+import View from "../View";
+import Text from "../../components/Text";
+import Button from "../../components/Button";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import Section from "../../components/Section";
+
+
+const NotFound: FC = () => {
+  return (
+    <View>
+      <Header />
+      <main >
+        <Section className="notfound-section" textAlignment="center">
+          <Text.Header.H1
+            text={ "404" }
+            size={ "large" }
+          />
+          <Text.Paragraph
+            className={ "notfound-msg" }
+            text={ "yup, there's definitely a mistake somewhere. couldn't find the requested resource 🙃" }
+          />
+          <Button.Link
+            to={ HOME }
+            text={ "go home" }
+            icon={ <TbHome /> }
+            className={ "go-home-cta" }
+          />
+        </Section>
+      </main>
+      <Footer />
+    </View>
+  );
+};
+
+
+export {
+  NotFound as default
+};
