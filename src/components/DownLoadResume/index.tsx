5b4c963527557c85dfdea76758d9e109fbaa61cd
Huboh <72440222+huboh@users.noreply.github.com>
2022-07-12 13:00:11 +0100

    🤙 REFACTOR: use new constant value in `DownloadResume` component

--- src/components/DownLoadResume/index.tsx
+++ src/components/DownLoadResume/index.tsx
@@ -4,7 +4,7 @@ import '../Button/variants/ButtonLink/button-link.scss';
 import { FC } from "react";
 import { TbFileDownload } from 'react-icons/tb';
 import { joinClassStrings } from '../../utils';
-import { ResumeDownloadLink, ResumeFileName } from "../../constants";
+import { Resume } from "../../constants";
 
 interface DownloadResumeProps {
   className?: string;
@@ -15,7 +15,7 @@ const DownloadResume: FC<DownloadResumeProps> = ({ className }) => {
   const classString = joinClassStrings("button button-link", className || "");
 
   return (
-    <a title={ "download resume" } className={ classString } href={ ResumeDownloadLink } download={ ResumeFileName }>
+    <a title={ "download resume" } className={ classString } href={ Resume.path } download={ Resume.fileName }>
       <span className="icon-wrapper">{ <TbFileDownload /> }</span>
       <span className='text-wrapper'> { "resume" } </span>
     </a>
