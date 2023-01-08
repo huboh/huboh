e87251834f4773181d8f3f4686a93b1f29486dc4
huboh <72440222+huboh@users.noreply.github.com>
2023-01-08 21:16:06 +0100

    create `Image` component wrapper around nextjs's

---
+++ components/Image/variants/Image/index.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import { default as NextImage, ImageProps as NextImageProps } from "next/image";
+
+// hooks
+import { useClassString } from "../../../../hooks";
+
+// styles
+import styles from "../../styles/image.module.scss";
+
+export interface ImageProps extends NextImageProps {
+  roundedBorder?: boolean;
+}
+
+const Image: FC<ImageProps> = (props) => {
+  const border = props.roundedBorder ? "round-border" : "";
+  const className = useClassString(styles.image, styles[border || "round-border"], props.className);
+
+  return (
+    <NextImage
+      { ...props }
+      className={ className }
+      placeholder={ props.placeholder || "blur" }
+    />
+  );
+};
+
+export {
+  Image as default
+};
---
+++ components/Image/variants/StyledImage/index.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+import { default as Image, ImageProps } from "../../variants/Image";
+
+// hooks
+import { useClassString } from "../../../../hooks";
+
+// styles
+import styles from "../../styles/image.module.scss";
+
+const StyledImage: FC<ImageProps> = (props) => {
+  const className = useClassString(styles.styled_image, props.className);
+
+  return (
+    <Image
+      { ...props }
+      className={ className }
+    />
+  );
+};
+
+export {
+  StyledImage as default
+};
