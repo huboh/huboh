dfdaede394a4ef049d1c2b07945818006e96f675
huboh <72440222+huboh@users.noreply.github.com>
2023-01-26 23:04:07 +0100

    REFACTOR: add `id` attribute to `Footer` component

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -7,19 +7,25 @@ import SocialIcons from "../SocialIcons";
 import ScrollToTop from "../ScrollToTop";
 
 // styles
-import styles from "./footer.module.scss";
+import styles from "./styles/footer.module.scss";
 import linkStyles from "../Text/styles/link.module.scss";
 
-const Footer: FC = () => {
+interface FooterProps {
+  id?: string;
+}
+
+const Footer: FC<FooterProps> = (props) => {
+  const id = props.id;
+  const className = styles["footer"];
   const paragraphText = (
     <>
       <span children="Designed & Built with 💖 by " />
-      <a className={ linkStyles.link } target="_blank" rel="noreferrer" children="Musa Knowledge" href={ Socials.twitter.link } />
+      <a className={ linkStyles.link } target="_blank" rel="noreferrer" children="Musa Knowledge" href={ Socials.linkedIn.link } />
     </>
   );
 
   return (
-    <footer className={ styles.footer }>
+    <footer id={ id } className={ className }>
       <SocialIcons />
       <Text.Paragraph size="small" children={ paragraphText } />
       <ScrollToTop />
