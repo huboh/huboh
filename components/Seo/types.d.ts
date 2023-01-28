b4fecc1574729ffaa3934ea18e9267806c2762d0
huboh <72440222+huboh@users.noreply.github.com>
2023-01-28 11:26:16 +0100

    REFACTOR: update `Seo` component & add more props

---
+++ components/Seo/types.d.ts
@@ -0,0 +1,10 @@
+export interface SeoProps {
+  title: string;
+  domain?: string;
+  ogType?: string;
+  ogImage?: string;
+  siteName?: string;
+  canonical?: string;
+  description?: string;
+  twitterHandle?: string;
+}
