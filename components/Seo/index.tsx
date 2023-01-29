210a9a197ab2327abec342e8190cf6309a428424
huboh <72440222+huboh@users.noreply.github.com>
2023-01-29 19:11:23 +0100

    REFACTOR: fix support for additional meta and link tags

--- components/Seo/index.tsx
+++ components/Seo/index.tsx
@@ -14,55 +14,69 @@ const Seo: FC<SeoProps> = (props) => {
       canonical={ props.canonical }
       description={ props.description }
       additionalMetaTags={ [
-        {
-          name: "theme-color",
-          content: preferences.colorScheme,
-          media: `(prefers-color-scheme: ${preferences.theme})`,
-        } as any,
-        {
-          name: "msapplication-TileColor",
-          content: preferences.colorScheme,
-        },
-        {
-          name: "msapplication-config",
-          content: "/favicon/browserconfig.xml",
-        }
-      ] }
+        ...(props.additionalMetaTags || []),
+        ...[
+          {
+            name: "theme-color",
+            content: preferences.colorScheme,
+            media: `(prefers-color-scheme: ${preferences.theme})`,
+          } as any,
+          {
+            name: "msapplication-TileColor",
+            content: preferences.colorScheme,
+          },
+          {
+            name: "twitter:image",
+            content: props.ogImage,
+          },
+          {
+            name: "twitter:image:alt",
+            content: props.ogAlt,
+          },
+          {
+            name: "msapplication-config",
+            content: "/favicon/browserconfig.xml",
+          }
+        ]]
+      }
       additionalLinkTags={ [
-        {
-          rel: "manifest",
-          href: "/manifest.json",
-        },
-        {
-          rel: "shortcut icon",
-          href: "/favicon.ico"
-        },
-        {
-          rel: "canonical",
-          href: (props.canonical ?? props.domain)!,
-        },
-        {
-          rel: "apple-touch-icon",
-          sizes: "180x180",
-          href: "/assets/images/apple-touch-icon.png",
-        },
-        {
-          rel: "icon",
-          type: "image/png",
-          sizes: "16x16",
-          href: "/assets/images/favicon-16x16.png",
-        },
-        {
-          rel: "icon",
-          type: "image/png",
-          sizes: "32x32",
-          href: "/assets/images/favicon-32x32.png",
-        },
-        {
-          rel: "mask-icon",
-          href: "/assets/images/safari-pinned-tab.svg",
-          color: preferences.colorScheme,
-        }
+        ...(props.additionalLinkTags || []),
+        ...[
+          {
+            rel: "manifest",
+            href: "/manifest.json",
+          },
+          {
+            rel: "shortcut icon",
+            href: "/favicon.ico"
+          },
+          {
+            rel: "canonical",
+            href: (props.canonical ?? props.domain)!,
+          },
+          {
+            rel: "apple-touch-icon",
+            sizes: "180x180",
+            href: "/assets/images/apple-touch-icon.png",
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            sizes: "16x16",
+            href: "/assets/images/favicon-16x16.png",
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            sizes: "32x32",
+            href: "/assets/images/favicon-32x32.png",
+          },
+          {
+            rel: "mask-icon",
+            href: "/assets/images/safari-pinned-tab.svg",
+            color: preferences.colorScheme,
+          }
+        ]
       ] }
       twitter={ {
         ...(props.twitter ?? {}),
@@ -99,6 +113,7 @@ Seo.defaultProps = {
   twitterHandle: process.env.NEXT_PUBLIC_TWITTER_HANDLE,
   description: "Explore the portfolio and read articles from Knowledge Musa, a professional software engineer specializing in building enterprise-ready web applications and mobile apps. Discover my skills and experience in front-end and back-end web development, as well as my knowledge of the latest industry trends and technologies. Learn from my articles and tutorials on various programming topics, and check out examples of my work in the projects section. Whether you're an individual or a business looking for a software engineer, you'll find valuable information here",
 
+  ogAlt: "knowledge musa's portfolio",
   ogType: "website",
   ogWidth: 1200,
   ogHeight: 630,
