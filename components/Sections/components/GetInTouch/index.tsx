0adc4ab4b19d0fea117257dbd8b21ab35fd3974c
huboh <72440222+huboh@users.noreply.github.com>
2023-05-20 12:33:21 +0100

    fix 🔧: remove typo in `GetInTouch` Component

--- components/Sections/components/GetInTouch/index.tsx
+++ components/Sections/components/GetInTouch/index.tsx
@@ -13,7 +13,7 @@ const GetInTouch: FC = () => {
   const id = "contact";
   const emailSubject = encodeURIComponent("Hey Knowledge, New Opportunity Awaits. Lets Talk!");
   const externalLink = `mailto:kenn1000kenn@gmail.com?importance=high&subject=${emailSubject}&body=`;
-  const paragraphText = "I'm currently available and more than happy to help build and launch your product and/or your startup. I could help take you or your team from idea/prototype to production while picking out tech stack, infra architecture and others. I could also help review architecture and/or code, you just name it. I would only take up roles where I am very I certain can provide enough human capital to you or your team.";
+  const paragraphText = "I'm currently available and more than happy to help build and launch your product and/or your startup. I could help take you or your team from idea/prototype to production while picking out tech stack, infra architecture and others. I could also help review architecture and/or code, you just name it. I would only take up roles where I am very certain I can provide enough human capital to you or your team.";
 
   return (
     <Section id={ id } textAlignment={ "center" } className={ styles.get_in_touch_section }>
